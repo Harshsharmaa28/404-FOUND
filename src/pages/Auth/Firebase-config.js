4ae485b63402117895f2d625cdafdfce8b7851b4
Dev <devsa2067@gmail.com>
2024-04-06 19:33:44 +0530

    Firebase intilized

---
+++ src/pages/Auth/Firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAz09UbQ1H6RniEPeMmxIbqmzY8vUpHIIc",
+  authDomain: "found-6fdaa.firebaseapp.com",
+  projectId: "found-6fdaa",
+  storageBucket: "found-6fdaa.appspot.com",
+  messagingSenderId: "96601614703",
+  appId: "1:96601614703:web:616d4de1d0a1fb10fda09a",
+};
+
+const app = initializeApp(firebaseConfig);
