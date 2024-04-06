40bfec292d5b68c506937da88429ec8651f57aad
Harsh Sharma <harshsharma0828@gmail.com>
2024-04-06 19:45:24 +0530

    Created Routes in Homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
+import { Routes, Route } from "react-router-dom";
 
+import About from "./pages/AboutUs/About";
+import Footer from "./components/Footer/Footer";
+import Homepage from "./components/Homepage/Homepage";
+import Scholarship from "./pages/Scholarship/Scholarship";
+import Mentors from "./pages/Mentors/Mentors";
+import SignIn from "./pages/Auth/SignIn";
 
 function App() {
-  
+  const links = [
+    {
+      path: "/about",
+      element: <About />,
+      id: 1,
+    },
+    {
+      path: "/footer",
+      element: <Footer />,
+      id: 2,
+    },
+    {
+      path: "scholarship",
+      element: <Scholarship />,
+      id: 4,
+    },
+    {
+      // path: "/faqs",
+      // element: <Faqs />,
+      // id: 5,
+    },
+    {
+      path: "/Mentors",
+      element: <Mentors />,
+      id: 6,
+    },
+    {
+      path: "/",
+      element: <Homepage />,
+      id: 7,
+    },
+    {
+      path: "/login",
+      element: <SignIn />,
+      id: 8,
+    },
+  ];
   return (
     <div>
-      
+      <Routes>
+        {links.map(({ path, element, id }) => (
+          <Route key={id} path={path} element={element} />
+        ))}
+      </Routes>
     </div>
   );
 }
