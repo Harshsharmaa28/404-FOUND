f45025224529350181ea944fbd9a32d40ccf4559
Harsh Sharma <harshsharma0828@gmail.com>
2024-04-06 19:11:10 +0530

    Added Homepage

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Landingpage from '../Landingpage/Landingpage'
+import Info from '../Info/Info'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Landingpage/>
+      <Info/>
+    </div>
+  )
+}
+
+export default Homepage
