5ad6ca0518c5326ef84cdae87041b795899d45bb
Harsh Sharma <harshsharma0828@gmail.com>
2024-04-06 22:23:07 +0530

    Infopage Updated

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Landingpage from "../Landingpage/Landingpage";
-// import Info from '../Info/Info'
+import Info from '../Info/Info'
 
 const Homepage = () => {
   return (
     <div>
       <Navbar />
       <Landingpage />
-      {/* <Info/> */}
+      <Info/>
     </div>
   );
 };
