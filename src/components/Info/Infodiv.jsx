6b60bb3d80ff744ee4d2c13e001eb18a3b6a218f
Dev <devsa2067@gmail.com>
2024-04-07 19:20:18 +0530

    Info content updated

--- src/components/Info/Infodiv.jsx
+++ src/components/Info/Infodiv.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-const Infodiv = () => {
+const Infodiv = ({ title, subTitle, color }) => {
   return (
-    <div className=" rounded-lg w-[15rem] h-[15rem] ">
-      <div className=" h-2/3 w-full bg-[#ECCDDC] "></div>
-      <div className=" h-1/3 text-center w-full  ">
-        <h1 className=" font-bold ">This is more about notes</h1>
-        <p className=" text-xs ">
-        Unleash your academic potential with our game-changing notes! and Unlock success with our library of effective study notes!
-
-        </p>
+    <div className="rounded-lg w-[15rem] h-[15rem]">
+      <div
+        className={`h-2/3 w-full `}
+        style={{
+          backgroundColor: color,
+          borderTopLeftRadius: "20px",
+          borderTopRightRadius: "20px",
+        }}
+      ></div>
+      <div className="h-1/3 text-center w-full">
+        <h1 className="font-bold">{title}</h1>
+        <p className="text-xs">{subTitle}</p>
       </div>
     </div>
   );
